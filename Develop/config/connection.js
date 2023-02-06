75caa521fcbdd94c1726fe0ed6b62610164abd89
wyamet <wyattdomanski.md@gmail.com>
2023-02-05 20:03:23 -0800

    Fixed dotenv naming error

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,13 +4,18 @@ const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
 
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,30 +5,27 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 
 // Categories have many Products
-
 Category.hasMany(Product, {
   foreignKey: "category_id",
+  onDelete: "SET NULL",
 });
 
 // Products belongToMany Tags (through ProductTag)
-
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "tag_name",
-  foreignKey: "tag_id",
+  // as: 'product_tag',
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
-
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "tag_name",
+  // as: 'product_tag',
   foreignKey: "tag_id",
 });
 
