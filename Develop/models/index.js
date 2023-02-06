f54308c477e435afa290c5762cc42d4de875b589
wyamet <wyattdomanski.md@gmail.com>
2023-02-05 19:02:59 -0800

    defined the relationships between categories, pproducts, tags, and productTags,

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,36 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+
 // Categories have many Products
+
+Category.hasMany(Products, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "tag_name",
+  foreignKey: "tag_id",
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "tag_name",
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
